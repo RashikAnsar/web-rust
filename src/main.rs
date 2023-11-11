463c1e707a73d887029f944c3f603c1f762e87b2
RashikAnsar <sk.ansar46@gmail.com>
2023-11-11 23:15:08 +0530

    feat: health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use web_rust_prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = "127.0.0.1:0";
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+    println!("server running on: http://{}:{}", address, port);
+    run(listener)?.await
 }
