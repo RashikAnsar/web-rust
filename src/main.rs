eef5de21835a81ab0743b2cb475faaf0d4260955
RashikAnsar <sk.ansar46@gmail.com>
2023-11-13 23:31:58 +0530

    feat: add observability

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 use std::net::TcpListener;
 
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
-use web_rust_prod::{configuration::get_configuration, startup::run};
+use web_rust_prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("web_rust_prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address.clone())?;
-    println!(
-        "server running on: http://{}:{}",
-        address, configuration.application_port
-    );
-    run(listener, connection_pool)?.await
+    println!("server running on: http://{}", address);
+    run(listener, connection_pool)?.await?;
+
+    Ok(())
 }
