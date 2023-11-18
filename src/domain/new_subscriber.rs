3792bce3bfef917243cf5d66880bd9b31020229b
RashikAnsar <sk.ansar46@gmail.com>
2023-11-18 21:48:37 +0530

    feat: add validation of form data

---
+++ src/domain/new_subscriber.rs
@@ -0,0 +1,9 @@
+// use super::SubscriberName;
+// use super::SubscriberEmail;
+use crate::domain::SubscriberEmail;
+use crate::domain::SubscriberName;
+
+pub struct NewSubscriber {
+    pub email: SubscriberEmail,
+    pub name: SubscriberName,
+}
